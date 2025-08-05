60ddcfa30795fa71c6f09967f4b0b77a26afce39
Reginaldo <reginaldobrain@gmail.com>
2025-08-05 10:20:00 -0300

    1019

--- park/src/pages/Servicos/Servicos.jsx
+++ park/src/pages/Servicos/Servicos.jsx
@@ -27,8 +27,8 @@ import React, { useState, useEffect } from "react";
 import "../Servicos/servicos.css";
 
 //const API_URL = "http://localhost:3000/servicos";
-const API_URL = "https://api-omega-sable.vercel.app/api/servicos";
-//const API_URL = import.meta.env.VITE_API_URL + "/servicos";
+//const API_URL = "https://api-omega-sable.vercel.app/api/servicos";
+const API_URL = import.meta.env.VITE_API_URL + "/servicos";
 
 
 
